e421a461da8476213ab9caba7bdb023a460134d6
Tais Torres <taisincollege@gmail.com>
2025-01-30 15:23:17 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,7 @@
+// Task 1 - Product Price Management
+let number = [12, 35, 20, 19, 9];
+number.push(23);
+// Added new price to array
+number.shift();
+// Removed the first price from array
+console.log("Updated Prices: ", number);
